d0238a5d5b597148ae6f6b94e31aa23f6b7267da
VladMB <cph-mb402@cphbusiness.dk>
2019-11-26 22:28:58 +0100

    csv publish works, but can't consume on route call

--- ParkAwayClient/routes.js
+++ ParkAwayClient/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRoutes = require('./routes/user');
 const parkRoutes = require("./routes/park");
+const csvRoutes = require("./routes/csv");
 const router = express.Router();
 
 router.use('/user', userRoutes);
 router.use('/park', parkRoutes);
+router.use('/csv', csvRoutes);
 
 module.exports = router;
--- ParkAwayClient/routes/csv.js
+++ ParkAwayClient/routes/csv.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const router = express.Router(); // eslint-disable-line new-cap
-var mq_client = require('../rpc/jsonclient');
+var util = require('util');
+var amqp =  require('amqp');
+var mq_cnn = amqp.createConnection({
+  host: '127.0.0.1'
+});
 
+// var _queue = null;
+var _consumerTag = null;
+var _message = "fok mai laif";
 
-router.get('/csv', function (req, res) {
-    
-    mq_client.connection.queue('csv_queue', function (q) {
-      q.subscribe(function (message) {
-        console.log('Got message', message);
-        queue.shift(false, false);
-      }).addCallback(function(res) {
-        // Hold on to the consumer tag so we can unsubscribe later
-        _consumerTag = res.consumerTag;
-      }).unsubscribe();
+router.get('/', function (req, res) {
+    mq_cnn.queue('csv_queue', function (q) {
+      console.log("got queue");
+      // _queue = queue;
+      // q.subscribe(function (message) {
+      //   console.log(message);
+      //   _message = message;
+      //   queue.shift(false, false);
+      //   res.status(200).json('heya');
+      // }).addCallback(function(res) {
+      //   // Hold on to the consumer tag so we can unsubscribe later
+      //   _consumerTag = res.consumerTag;
+      //   res.status(200).json('heya');
+      // });
+      // q.unsubscribe(_consumerTag);
     });
+
+    // res.status(200).json(_message);
 });
 
 module.exports = router;
